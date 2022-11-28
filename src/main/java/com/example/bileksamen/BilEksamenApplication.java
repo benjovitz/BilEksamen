75353d1bf4446ef230be2ac2ba617a03ddba8b70
benjovitz <dbenjovitz@gmail.com>
2022-11-28 10:56:46 +0100

    first

---
+++ src/main/java/com/example/bileksamen/BilEksamenApplication.java
@@ -0,0 +1,13 @@
+package com.example.bileksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilEksamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilEksamenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bileksamen/BilEksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bileksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilEksamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
